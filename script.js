0c4393b9850267bc2d4944ede1937f8a0f9954c6
cl.lee <cl.lee.ofz@gmail.com>
2023-01-06 18:51:34 +0000

    moved prompt and confirm out of getPasswordOptions function

--- script.js
+++ script.js
@@ -90,19 +90,20 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let passwordLength = prompt("Please enter the length of password, it should be between 10 to 64 characters.");
-  let lowercaseOption = confirm("Include lowercase characters? (Click OK for yes, Cancel for no)");
-  let uppercaseOption = confirm("Include uppercase characters? (Click OK for yes, Cancel for no)");
-  let numericOption = confirm("Include numeric characters? (Click OK for yes, Cancel for no)");
-  let specialCharOptions = confirm("Include special characters? (Click OK for yes, Cancel for no)");
-  return;
+
 } 
+
+const passwordLength = prompt("Please enter the length of password, it should be between 10 to 64 characters.");
+const lowercaseOption = confirm("Include lowercase characters? (Click OK for yes, Cancel for no)");
+const uppercaseOption = confirm("Include uppercase characters? (Click OK for yes, Cancel for no)");
+const numericOption = confirm("Include numeric characters? (Click OK for yes, Cancel for no)");
+const specialCharOptions = confirm("Include special characters? (Click OK for yes, Cancel for no)");
 
 let builtArray = "";
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  
 }
 
 // Function to generate password with user input
